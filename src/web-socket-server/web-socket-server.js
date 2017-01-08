c3a5316dc9198770d22e33be42dd0b8a648bed6b
Bartosz Jakubowiak <bartosz@jakubowiak.pl>
2017-01-08 13:12:54 +0100

    Adjust auth logic with new remote specification

--- src/web-socket-server/web-socket-server.js
+++ src/web-socket-server/web-socket-server.js
@@ -14,7 +14,7 @@ export default class webSocketServer {
 
     this.socketIo = socketIo(server);
     this.db = db;
-    this.devices = {};
+    this.remotes = {};
 
     this.socketIo.use((socket, next) => {
       return this._authMiddleware(socket, next)
@@ -32,11 +32,14 @@ export default class webSocketServer {
    */
   _authMiddleware(socket, next) {
 
-
-    this.db.each(`SELECT * FROM devices WHERE \`device-id\`= ?`, socket.handshake.query.deviceId,(err, row) => {
+    this.db.get(`SELECT * FROM devices WHERE \`device-id\`= ?`, socket.handshake.query.remoteId,(err, row) => {
 
       if (err) {
-        return next(new Error('Authentication error'));
+        return next(new Error('Authentication error: database error'));
+      }
+
+      if (!row) {
+        return next(new Error('Authentication error: remote not found'));
       }
 
       try {
@@ -45,15 +48,14 @@ export default class webSocketServer {
 
         var decrypted = crypto.publicDecrypt(row.pem, new Buffer(handshake, "base64")).toString("utf8");
 
-        if (socket.handshake.query.handshakeTest === decrypted) {
+        if (socket.handshake.query.verificationString === decrypted) {
           return next();
         } else {
-          return next(new Error('Authentication error'));
+          return next(new Error('Authentication error: handshake error'));
         }
 
       } catch (error) {
-        console.log(error);
-        return next(new Error('Authentication error'));
+        return next(new Error('Authentication error: bad key pair'));
       }
 
     });
@@ -68,10 +70,12 @@ export default class webSocketServer {
 
     this.socketIo.on('connection', socket => {
 
-      this.devices[socket.handshake.query.deviceId] = socket;
+      this.remotes[socket.handshake.query.remoteId] = socket;
+      console.log(`Remote connected: ${socket.handshake.query.remoteId}`);
 
       socket.on('disconnect', () => {
-        delete this.devices[socket.handshake.query.deviceId];
+        console.log(`Remote had disconnected: ${socket.handshake.query.remoteId}`);
+        delete this.remotes[socket.handshake.query.remoteId];
       });
 
     });
