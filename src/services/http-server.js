89560c57ddb8305b46dfad6edbf6bc24eb037b91
Bartosz Jakubowiak <bartosz@jakubowiak.pl>
2017-01-31 08:37:24 +0100

    remove letsencrypt integration from server

--- src/services/http-server.js
+++ src/services/http-server.js
@@ -73,12 +73,6 @@ expressServer.use(bodyParser.urlencoded({
 
 expressServer.use(bodyParser.json());
 
-/**
- * Letsencrypt validation
- */
-expressServer.get('/.well-known', (req, res) => {
-  res.send('ok');
-});
 
 server.listen(port, () => {
   console.info(`Server had started on localhost:${port}`);
